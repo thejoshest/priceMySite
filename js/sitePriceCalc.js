5cefb2f63445c3033c01b22252673926f8f8b400
Joshua Hooper <jhoop.acb@gmail.com>
2017-05-16 23:13:52 -0400

    turned code into jQuery plugin

--- js/sitePriceCalc.js
+++ js/sitePriceCalc.js
@@ -1,68 +1,87 @@
 /**
  *
     SiteStat.js
-    Version:   V.1.1.0
+    Version:   V.1.2.0
     Author:    Joshua Hooper
  *
 **/
-$(function(){
-  var siteCost = 500,
-      pageTotal = 0;
+/** TODO: create a .gitignore before continuing work **/
+;(function($){
 
-  function siteCalc(element) {
+    $.fn.siteCalc = function(options) {
+          
+            var element = $(this);
 
-      var output = '<div class="calculator-body">';
+            var defaults = $.extend({
+              siteCost : 500,
+              siteCostConst : 500,
+              pageTotal : 0
+            }, options);
 
-        output += '<div class="sc-total"><p>Your Sites\' Cost</p><p>$<span id="cost">' + siteCost + '</span></p></div>';
+            var buildCalc = function(elem) {
 
-        output += '<form name="calcForm" id="siteCalcForm" method="post">';
-          output += '<div class="calc-inputs">';
+              output = '<div class="calculator-body">';
 
-            output += '<h5>Pages:</h5>';
-            output += '<input type="number" name="pages" id="pages" class="calculate"><span>help!</span>';
+              output += '<div class="sc-total"><p>Your Sites\' Cost</p><p>$<span id="cost">' + defaults.siteCost + '</span></p></div>';
 
-            output += '<h5>SEO:</h5>';
-            output += '<input type="checkbox" name="pages" value="seo"><span>help!</span>';
+              output += '<form name="calcForm" id="siteCalcForm" method="post">';
+                output += '<div class="calc-inputs">';
 
-            output += '<h5>Design:</h5>';
-            output += '<select name="design" class="calculate">';
-              output+= '<option value="default">Select One</option>';
-              output+= '<option value="custom">Fully Custom</option>';
-              output+= '<option value="partialCustom">Partially Custom</option>';
-              output+= '<option value="template">Premade-Template</option>';
-            output += '</select><span>help!</span>';
+                  output += '<h5>Pages:</h5>';
+                  output += '<input type="number" name="pages" id="pages" class="calculate pages"><span>help!</span>';
 
-            output += '<h5>Platform Type:</h5>';
-            output += '<select name="design" class="calculate">';
-              output+= '<option value="default">Select One</option>';
-              output+= '<option value="ecommerce">E-Commerce</option>';
-              output+= '<option value="partialCustom"></option>';
-              output+= '<option value="template">Premade-Template</option>';
-            output += '</select><span>help!</span>';
+                  output += '<h5>SEO:</h5>';
+                  output += '<input type="checkbox" name="pages" class="seo" value="seo"><span>help!</span>';
 
-          output += '</div>';
-        output += '</form>';
-      output += '</div>';
+                  output += '<h5>Design:</h5>';
+                  output += '<select name="design" class="design">';
+                    output+= '<option value="default">Select One</option>';
+                    output+= '<option value="custom">Fully Custom</option>';
+                    output+= '<option value="partialCustom">Partially Custom</option>';
+                    output+= '<option value="template">Premade-Template</option>';
+                  output += '</select><span>help!</span>';
 
-      return document.getElementById(element).innerHTML = output;
+                  output += '<h5>Platform Type:</h5>';
+                  output += '<select name="design" class="platform">';
+                    output+= '<option value="default">Select One</option>';
+                    output+= '<option value="ecommerce">E-Commerce</option>';
+                    output+= '<option value="partialCustom"></option>';
+                    output+= '<option value="template">Premade-Template</option>';
+                  output += '</select><span>help!</span>';
 
+                output += '</div>';
+              output += '</form>';
+              output += '</div>';
+
+              return elem.html(output);
+            }
+
+        buildCalc(element);
+        $('.calculate').change(calcSite);
+
+        function calcSite() {
+          var pagesTotal = 0,
+              designType = 0,
+              platType = 0;
+          if (this.hasClass('pages')) {
+            var pagesTotal = 0,
+                pageCost = 100,
+                pagesInput = $('#pages').val();
+                pagesTotal = pagesInput * pageCost;
+          } else if (this.hasClass('seo')) {
+
+          } else if (this.hasClass('design')) {
+
+          } else if (this.hasClass('platform')) {
+
+          } else {};
+
+          defaults.siteCost = defaults.siteCostConst + pagesTotal;
+          $('#cost').html(defaults.siteCost);
+
+        }
     };
-  siteCalc('site-calculator');
 
-  $('.calculate').change(calculateSite);
+}(jQuery));
 
-  function calculateSite() {
-    var pagesTotal = 0,
-        pagesInput = $('#pages').val();
-
-    function pageTotal() {
-        pagesTotal = pagesInput * 100;
-        return pagesTotal;
-    };
-    pageTotal();
-    siteCost += pagesTotal;
-    $('#cost').html(siteCost);
-    console.log(siteCost);
-  };
-
-});
+$('.site-calculator').siteCalc();
